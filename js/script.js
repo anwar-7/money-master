71f9114f8e8012f8baa4c8916ef667020e1c7913
anwarul1 <anwarjewel007@gmail.com>
2022-02-17 17:46:03 +0600

    function complit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
-/*
-// button click field
-// calculate-btn
-// save-btn
- */
 // foo expenses function
 function foodExpense() {
   const inputFoodExpense = document.getElementById('food-expenses');
   const foodCost = parseFloat(inputFoodExpense.value);
-  if (isNaN(foodCost) || foodCost < 0) {
-    // return alert('Please input valid amount of money in number format');
-  }
   return foodCost;
 }
 
@@ -17,9 +9,6 @@ function foodExpense() {
 function rentExpense() {
   const inputRentExpense = document.getElementById('rent-expenses');
   const rentCost = parseFloat(inputRentExpense.value);
-  if (isNaN(rentCost) || rentCost < 0) {
-    // return alert('Please input valid amount of money in number format');
-  }
   return rentCost;
 }
 
@@ -27,9 +16,6 @@ function rentExpense() {
 function clothesExpense() {
   const inputClothesExpense = document.getElementById('clothes-expenses');
   const clothesCost = parseFloat(inputClothesExpense.value);
-  // if (isNaN(clothesCost) || clothesCost < 0) {
-  //   // return alert('Please input valid amount of money in number format');
-  // }
   return clothesCost;
 }
 
@@ -53,28 +39,65 @@ function totalBalance() {
 }
 
 // clicked hear expenses
-document
-  .getElementById('calculate-btn')
-  .addEventListener('click', function (event) {
-    // event.preventDefault();
+document.getElementById('calculate-btn').addEventListener('click', function () {
+  calculateError();
+});
+
+// clicked hear saving
+document.getElementById('save-btn').addEventListener('click', function (event) {
+  saveError();
+});
+
+function calculateError() {
+  let totalCost = totalExpenses();
+  let income = document.getElementById('input-income').value;
+  let totalIncome = parseFloat(income);
+  let foodExpense = document.getElementById('food-expenses').value;
+  let rentExpense = document.getElementById('rent-expenses').value;
+  let clothesExpense = document.getElementById('clothes-expenses').value;
+
+  if (income == '' || income == null) {
+    alert('please input nambar valu');
+    return false;
+  } else if (foodExpense == '' || foodExpense == null) {
+    alert('please input nambar valu');
+    return false;
+  } else if (rentExpense == '' || rentExpense == null) {
+    alert('please input nambar valu');
+    return false;
+  } else if (clothesExpense == '' || clothesExpense == null) {
+    alert('please input nambar valu');
+    return false;
+  } else if (totalIncome < totalCost) {
+    alert('total cost can not be greater than total income');
+  } else {
     const totalExpensesOutput = document.getElementById('expenses-total');
     totalExpensesOutput.innerText = totalExpenses();
     const balanceExpensesOutput = document.getElementById('balance-total');
     balanceExpensesOutput.innerText = totalBalance();
-  });
+  }
+}
 
-// clicked hear saving
-document.getElementById('save-btn').addEventListener('click', function (event) {
-  const savingPercent = document.getElementById('saving-percent').value;
-  const saving = (income() * parseFloat(savingPercent)) / 100;
+function saveError() {
+  let balance = totalBalance();
+  let saveAmount = document.getElementById('saving-percent').value;
+  let totalSaveAmount = parseFloat(saveAmount);
 
-  // saving amount show
-  const savingAmount = document.getElementById('saving-amount');
-  savingAmount.innerText = saving;
+  if (saveAmount == '' || saveAmount == null) {
+    alert('please input nambar valu');
+    return false;
+  } else if (balance < totalSaveAmount) {
+    alert('total saving can not be greater than total Balance');
+  } else {
+    const savingPercent = document.getElementById('saving-percent').value;
+    const saving = (income() * parseFloat(savingPercent)) / 100;
 
-  // remaining balance show
-  const remainingBalance = document.getElementById('remaining-balance');
-  remainingBalance.innerText = totalBalance() - saving;
-});
+    // saving amount show
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = saving;
 
-function
+    // remaining balance show
+    const remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance.innerText = balance - saving;
+  }
+}
